115574c1a31a4be7682d085757ed3d5e1848078e
Lalith <lalithr95@gmail.com>
2016-02-03 20:04:18 +0530

    update

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,14 @@ cd /app
 echo "Setting up SSH"
 mkdir /app/.ssh
 echo "$SSH_PRIVATE_KEY" > .ssh/id_rsa
-#echo "Host heroku.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+echo "Host heroku.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
 echo "Cloning ..."
 git clone $APPNAME_GITHUB_URL
 cd /app/Github-visualization
 echo "Adding Heroku URL"
 git remote add prod $APPNAME_PRODUCTION_URL
-git config --global url.ssh://git@heroku.com/.insteadOf https://git.heroku.com/
+# git config --global url.ssh://git@heroku.com/.insteadOf https://git.heroku.com/
 echo "Pushing to Heroku"
 git checkout master
 git push prod master

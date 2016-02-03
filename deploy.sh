120206499d4ceaed1decdac7fbebd32537fa663d
Lalith <lalithr95@gmail.com>
2016-02-03 20:02:18 +0530

    comment

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ cd /app
 echo "Setting up SSH"
 mkdir /app/.ssh
 echo "$SSH_PRIVATE_KEY" > .ssh/id_rsa
-echo "Host heroku.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+#echo "Host heroku.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
 echo "Cloning ..."
 git clone $APPNAME_GITHUB_URL
